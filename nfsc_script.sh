8cc323683bab5f687ff40811fb88e9d22e1de1c3
mihas174 <74105900+mihas174@users.noreply.github.com>
2020-12-01 15:39:55 +0500

    Update nfsc_script.sh

--- nfsc_script.sh
+++ nfsc_script.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-192.168.88.132:/usr/local/nfs    /mnt   nfs    rsize=8192,wsize=8192,rw,sync > /etc/fstab
+192.168.50.10:/usr/local/nfs    /mnt   nfs    rsize=8192,wsize=8192,rw,sync > /etc/fstab
